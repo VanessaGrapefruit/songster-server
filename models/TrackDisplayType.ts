280cac01d1070cad2c66b07901dd4cc7e7d0032c
VanessaGrapefruit <16lexa1602@gmail.com>
2021-01-08 03:40:53 +0300

    feat: add types to convert from midi

---
+++ models/TrackDisplayType.ts
@@ -0,0 +1,44 @@
+import { Duration, Clef, Size, Touch, NoteName, Alteration } from './Notations';
+
+export interface Song {
+    Tracks: Track[];
+    Name: string;
+    Author: string;
+    Difficulty: number;
+}
+
+export interface Track {
+    Instrument: string;
+    Name?: string;
+    Author?: string;
+    Sections: Section[];
+}
+
+export interface Section {
+    Bpm: number;
+    Key: NoteName;
+    Clef: Clef;
+    Size: Size;  //number of beats per measure
+    Measures: Measure[];
+}
+
+export interface Measure {
+    Id: number;
+    Time: number;
+    Notes: Chord[];  // sum of all notes durations must be equal to size!!!
+}
+
+export interface Chord {
+    Name?:string;
+    Notes: Note[];
+}
+
+export interface Note {
+    Name?: NoteName;
+    Alteration: Alteration;
+    Octave?: number;
+    Duration: Duration;
+    IsDotted: boolean;
+    IsPause: boolean;
+    Touch?: Touch;
+}
