41bc0b07413818cb5e00e7624658ea47ce2b7cf6
Ilya Barachenia <ilya.barachenia@gmail.com>
2021-01-26 21:36:28 +0100

    'resolveconflicts'

--- controllers/songController.ts
+++ controllers/songController.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import path from "path";
 import { MidiConverter } from '../utils/midi-converter/midiConverter';
 import { SearchOptions } from '../viewModels/SearchOptions';
+import { Midi } from '@tonejs/midi'
 
 function saveItemIfNotExist(model, modelObj): void {
     model.findOne({name: modelObj.name}, async function (err, mod) {
@@ -17,16 +18,59 @@ function saveItemIfNotExist(model, modelObj): void {
 }
 
 //http://localhost:3000/songs/?name=Enter Sandman&author=Metallica
-export function FindSongs(req: express.Request, res: express.Response): void {
-    console.log('Get songs');
-    const options: SearchOptions = req.query;
-    Song.find(options,{name:1,author:1,difficulty:1})
-        .exec(function (err, result) {
-            if (err) res.sendStatus(404);
+// export function FindSongs(req: express.Request, res: express.Response): void {
+//     console.log('Get songs');
+//     const options: SearchOptions = req.query;
+//     Song.find(options,{name:1,author:1,difficulty:1})
+//         .exec(function (err, result) {
+//             if (err) res.sendStatus(404);
+//
+//             res.send(result);
+//         });
+// };
 
+export async function FindSongs(req: express.Request, res: express.Response) {
+    const options = defineSearchOptions(req);
+    const instrument = req.query.instrument ? req.query.instrument.toString() : '';
+    const queryString = req.query.name ? req.query.name.toString().toLowerCase() : '';
+
+    Song.find(options).select('name author difficulty')
+        .exec(function(err,songs) {
+            if(err) res.status(404).send("Songs don't founded");
+
+            const result = songs.filter((song:any) => {
+                const isInstrument = isInstrumentDefined(song,instrument);
+                const isQueryString = isQueryStringMatchSong(song,queryString);
+                return (isInstrument && isQueryString);
+            })
             res.send(result);
-        });
+        })
 };
+
+function defineSearchOptions(req: express.Request) {
+    let options = {}
+    const keys = ['genre','difficulty'];
+    for (const key of keys) {
+        if(!req.query[key]) continue;
+        options[key] = req.query[key];
+    }
+    return options;
+}
+
+function isInstrumentDefined(song:any, instrument: string) {
+    const instruments: string[] = song.instruments;
+    if(!instruments) return true;
+    return instruments.includes(instrument);
+}
+
+function isQueryStringMatchSong(song: any, quaryString: string) {
+    if(!quaryString) return true;
+    const str = [song.author,song.name].join(' ').toLowerCase();
+    for(const word of quaryString.split(' ')) {
+        if(!str.includes(word)) return false;
+    }
+    return true;
+}
 
 //http://localhost:3000/songs/id/?id=6000521b6a4f1508a4233e03
 export function FindSongById(req:express.Request, res: express.Response) {
@@ -53,17 +97,21 @@ export function addSongPage(req: express.Request, res: express.Response) {
 
 export async function addSong(req, res: express.Response) {
     console.log('add file to db');
+
     const midiData = req.file.buffer;
+    const midi = new Midi(midiData);
+    const instruments = midi.tracks.map(track => track.instrument.name);
 
     let songObj = {
         name: req.body.name,
         author: req.body.author,
         genre: req.body.genre,
         midi: midiData,
+        instruments
     };
     const song = new Song(songObj);
     await song.save();
-    
+
     res.redirect(`/songs/id/?id=${song.id}`);
 }
 
--- viewModels/SearchOptions.ts
+++ viewModels/SearchOptions.ts
@@ -1,7 +1,15 @@
+
 export interface SearchOptions {
     name?: string;
     author?: string;
     genre?: string
     instruments?: string[];
+    difficulty?: string;
+}
+export interface SearchOptions {
+    //name?: string;
+    //author?: string;
+    genre?: string
+    //instruments?: string[];
     difficulty?: string;
 }
