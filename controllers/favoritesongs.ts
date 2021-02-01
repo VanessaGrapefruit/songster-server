8705bc05d5457b5aecc3a991320513c82b420082
Ilya Barachenia <ilya.barachenia@gmail.com>
2021-02-01 08:45:41 +0100

    feat: add fav songs list controller

--- controllers/favoritesongs.ts
+++ controllers/favoritesongs.ts
@@ -1,22 +1,21 @@
 import User from '../models/User';
 import Song from '../models/Song';
 import mongoose from "mongoose";
+import express from "express";
 
 
 
-export async function favorite_add(req, res) {
+export async function favorite_add(req: express.Request, res: express.Response) {
 
-    const loggedUser = await User.findById(req.query.idUser, function (err, user) {
+    const loggedUser = await User.findById(req.body.userId, function (err, user) {
         if(err) res.sendStatus(404);
     })
 
-    const song = await Song.findById(req.query.idSong, function (err, user) {
+    const song = await Song.findById(req.body.songId, function (err, user) {
         if(err) res.sendStatus(404);
     })
 
-    const isInArray = loggedUser.favoriteSongs.some(function (song) {
-        return song.equals(song._id);
-    })
+    const isInArray = loggedUser.favoriteSongs.includes(song._id);
 
     if (!isInArray) {
         loggedUser.favoriteSongs.push(song)
@@ -28,11 +27,11 @@ export async function favorite_add(req, res) {
 
 export async function favorite_delete (req, res) {
 
-    const loggedUser = await User.findById(req.query.idUser, function (err, user) {
+    const loggedUser = await User.findById(req.body.userId, function (err, user) {
         if(err) res.sendStatus(404);
     })
 
-    const song = await Song.findById(req.query.idSong, function (err, user) {
+    const song = await Song.findById(req.body.songId, function (err, user) {
         if(err) res.sendStatus(404);
     })
 
@@ -45,4 +44,17 @@ export async function favorite_delete (req, res) {
     }
 
     res.end()
+}
+
+export async function favorite_songs_list (req: express.Request, res: express.Response) {
+
+    const loggedUser = await User.findById(req.query.userId, function (err, user) {
+        if(err) res.sendStatus(404);
+    })
+
+    const favSongsArray = await loggedUser.favoriteSongs;
+
+    const result = await Song.find({_id:{$in:[...favSongsArray]}});
+
+    res.send(result);
 }
--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -1,6 +1,6 @@
 import {Router} from 'express';
 import {singup_get, login_get, singup_post, login_post, logout_get} from '../controllers/authController';
-import {favorite_add, favorite_delete} from '../controllers/favoritesongs'
+import {favorite_add, favorite_delete, favorite_songs_list} from '../controllers/favoritesongs'
 const router = Router();
 
 router.get('/signup', singup_get);
@@ -10,5 +10,6 @@ router.post('/login', login_post);
 router.get('/logout', logout_get);
 router.post('/favorite-songs-add', favorite_add);
 router.post('/favorite-songs-delete', favorite_delete);
+router.get('/favorite-songs', favorite_songs_list);
 
 export default router;
